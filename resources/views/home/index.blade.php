3ef0aa62abbfa761a4035ea26f77b1de0633e52e
megumi seki <sggmm712@gmail.com>
2025-04-05 14:26:40 +0900

    Update file used to learn basic blade directives

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,15 +1,61 @@
 <h1>Hello From Laravel Course</h1>
 
-<script>
-    const hobbies = {{ Js::from($hobbies) }}
-</script>
+<!-- this comment is visible -->
+{{-- This comment is invisible --}}
 
-@verbatim
-<div>
-    Name: {{ name }}
-    Age: {{ age }}
-    Job: {{ job }}
-    Hobbies: {{ hobbies }}
-    @if
-</div>
-@endverbatim
+{{-- every directive needs to be closed --}}
+@if (false) This will be displayed @endif
+@unless () 
+@isset()
+@empty()
+@auth {{--check if user is authenticated--}}
+@guest {{--check if user is guest--}}
+@switch()
+    @case()
+        @break
+    @case()
+        @break
+    @default
+@endswitch
+
+@for ($i =0; $i < 5; $i++)
+    <p> {{ $i + 1 }} </p>
+@endfor
+
+@foreach ($cars as $car)
+    <p>Model: {{ $car->model }}</p>
+@endforeach
+
+@forelse ($cars as $car)
+    <p>Model: {{ $car->model }}</p>
+@empty
+    <p>There are no cars</p>
+@endforelse
+
+@while (false)
+    
+@endwhile
+
+@foreach ([1, 2, 3, 4, 5] as $num)
+    @if ($num == 2)
+        @continue
+    @endif
+    <p>{{$num}}</p>
+@endforeach
+
+@foreach ([1, 2, 3, 4, 5] as $num)
+    @continue($num ==2)
+    <p>{{$num}}</p>
+@endforeach
+
+@foreach ([1, 2, 3, 4, 5] as $num)
+    @if ($num == 4)
+        @break
+    @endif
+    <p>{{$num}}</p>
+@endforeach
+
+@foreach ([1, 2, 3, 4, 5] as $num)
+    @break($num == 4)
+    <p>{{$num}}</p>
+@endforeach
