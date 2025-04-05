28db549b8027d3a4b661888220f920ab77268c2e
megumi seki <sggmm712@gmail.com>
2025-04-05 17:24:40 +0900

    Update files used to learn layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield("title") | {{ config("app.name", "Laravel") }}</title>
-</head>
-<body>
-    <header>Your Header</header>
-    @yield("content")
-    <footer>Your Footer</footer>    
-</body>
-</html>
+@extends("layouts.clean")
+
+
+
+    @section("childContent")
+        @include("layouts.partials.header")
+        @yield("content")
+        <footer></footer>
+    @endsection 
+
+
