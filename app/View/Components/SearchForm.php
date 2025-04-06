fc9f549862d7e8bace6f407e1e1e1e90559afee6
megumi seki <sggmm712@gmail.com>
2025-04-06 21:49:03 +0900

    Update changes used to learn about database

--- app/View/Components/SearchForm.php
+++ app/View/Components/SearchForm.php
@@ -12,8 +12,7 @@ class SearchForm extends Component
      * Create a new component instance.
      */
     public function __construct(
-        public string $action = "/search",
-        public string $method = "GET"
+        
     )
     {
         //
