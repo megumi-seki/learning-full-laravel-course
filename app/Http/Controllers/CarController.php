83bb2a84ea95a35da1335486c593a00ece6e396a
megumi seki <sggmm712@gmail.com>
2025-04-09 17:20:15 +0900

    Update files to complete search blade and learn Ordering Data

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -65,6 +65,42 @@ class CarController extends Controller
 
     public function search()
     {
-        return view("car.search");
+        $query = Car::where("published_at", "<", now())
+            ->orderBy("published_at", "desc");
+        $carCount = $query->count();
+        $cars = $query->limit(30)->get();
+        return view("car.search", ["cars" => $cars, "carCount" => $carCount]);
     }
 }
+
+
+/*
+// Ordering Data
+// Order by multiple columns
+Car::orderBy("published_at", "desc")
+    ->orderBy("price", "asc")
+    ->get();
+
+// Using predefined latest() and oldest() methods
+Car::latest()->get(); //The same as
+Car::orderBy("created_at", "desc")->get();
+
+Car::oldest()->get(); //The same as
+Car::orderBy("created_at", "asc")->get();
+
+Car::latest("published_at")->get(); //The same as
+Car::orderBy("published_at", "desc")->get();
+
+// Select in random order
+// Select cars in random order
+$cars = Car::inRandomOrder()->get();
+
+// Remove ordering
+$query = Car::orderBy("published_at", "desc");
+// Remove ordering or reorder
+$cars = $query->reorder() // removes existing ordering
+            ->orderBy("price") // applies new ordering
+            ->get();
+
+// Or you can also do
+$cars = $query->reorder("price")->get();*/
